c2798ddd2476bd7c40f86831966d1c5f05cc598e
hamza12022skipq <hiqbal.bscs18seecs@seecs.edu.pk>
2023-02-21 23:36:19 +0500

    added functionality for fetching products while using sql database

--- models/product.js
+++ models/product.js
@@ -1,22 +1,4 @@
-const fs = require("fs");
-const path = require("path");
-const Cart = require("./cart");
-
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
-);
-
-const getProductsFromFile = (cb) => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
+const db = require("../util/database");
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -27,45 +9,13 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {
-    getProductsFromFile((products) => {
-      if (this.id) {
-        const exisingProductIndex = products.findIndex((p) => p.id === this.id);
-        const updatedProducts = [...products];
-        updatedProducts[exisingProductIndex] = this;
-        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-          console.log(err);
-        });
-      } else {
-        this.id = Math.random();
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), (err) => {
-          console.log(err);
-        });
-      }
-    });
+  save() {}
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM products");
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
-  }
+  static deleteById() {}
 
-  static deleteById(id) {
-    getProductsFromFile((products) => {
-      const product = products.find((p) => p.id == id);
-      const updatedProduct = products.filter((p) => p.id != id);
-      fs.writeFile(p, JSON.stringify(updatedProduct), (err) => {
-        if (!err) {
-          Cart.deleteProduct(id, product.price);
-        }
-      });
-    });
-  }
-
-  static findById(id, cb) {
-    getProductsFromFile((products) => {
-      const product = products.find((p) => p.id == id);
-      cb(product);
-    });
-  }
+  static findById() {}
 };
