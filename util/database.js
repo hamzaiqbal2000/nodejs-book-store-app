d0126919d8e165f43850ed4359369f234fbf11f1
hamza12022skipq <hiqbal.bscs18seecs@seecs.edu.pk>
2023-02-22 10:28:25 +0500

    connnected to database using sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("nodeproject", "root", "root123", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "nodeproject",
-  password: "root123",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
