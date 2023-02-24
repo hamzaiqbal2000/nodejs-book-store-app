ae33dbf161a13683b9e00c767cc9e136439309d6
hamza12022skipq <hiqbal.bscs18seecs@seecs.edu.pk>
2023-02-25 00:22:05 +0500

    fetching admin products

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,16 +16,26 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   console.log("id " + prodId);
-  Product.findByPk(prodId)
+  Product.findAll({ where: { id: prodId } })
     .then((product) => {
       console.log("PRODUCT ", JSON.stringify(product));
       res.render("shop/product-detail", {
-        product: product,
-        pageTitle: "product.title",
+        product: product[0],
+        pageTitle: "product[0].title",
         path: "/products",
       });
     })
     .catch((err) => console.log(err));
+  // Product.findByPk(prodId)
+  //   .then((product) => {
+  //     console.log("PRODUCT ", JSON.stringify(product));
+  //     res.render("shop/product-detail", {
+  //       product: product,
+  //       pageTitle: product.title,
+  //       path: "/products",
+  //     });
+  //   })
+  //   .catch((err) => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
