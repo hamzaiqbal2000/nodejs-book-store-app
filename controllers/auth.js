20d51e50d284cd7f621e56baa0cc6bfdaa68a8d5
hamza12022skipq <hiqbal.bscs18seecs@seecs.edu.pk>
2023-04-02 22:54:35 +0500

    added cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,15 @@
 exports.getLogin = (req, res, next) => {
+  const LoggedIn =
+    req.get("Cookie")?.split(";")[0]?.trim()?.split("=")[1] === "true";
+  console.log(req.get("Cookie"));
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: req.LoggedIn,
+    isAuthenticated: LoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.LoggedIn = true;
+  res.setHeader("Set-Cookie", "loggedIn=true");
   res.redirect("/");
 };
